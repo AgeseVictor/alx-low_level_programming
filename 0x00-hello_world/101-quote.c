0c55e153873c078a2677de3da007996b16353ce5
AgeseVictor <agesevictor@gmail.com>
2022-06-30 20:14:59 -0700

    betty my friend

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
+
 #include <stdio.h>
-
-
+#include <string.h>
+#include <unistd.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 1 (Success)
+ *  * main -Entry point
+ *   * Description: 'A new project abot using puts with special symbols'
+ *    * Return: Always 1 (success)
  */
-
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - \
-			Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
