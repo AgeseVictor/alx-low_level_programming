8dd00fd6b6df90eafdb588c77b3495b34f9a9222
AgeseVictor <agesevictor@gmail.com>
2022-06-30 19:51:49 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - \
+			Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
