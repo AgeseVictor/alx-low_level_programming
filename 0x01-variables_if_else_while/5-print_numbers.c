80c3d63d2d240bdc9f9d1b1054a28ea4f4645d09
AgeseVictor <agesevictor@gmail.com>
2022-07-02 06:57:07 -0700

    5alive

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,20 +7,20 @@
  */
 int main(void)
 {
-	int c;
+int c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		putchar(c);
+for (c = '0'; c <= '9'; c++)
+{
+	putchar(c);
 
-		if (c != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+if (c != '9')
+{
+	putchar(',');
+	putchar(' ');
+}
+}
 
 	putchar('\n');
 
-	return (0);
+return (0);
 }
