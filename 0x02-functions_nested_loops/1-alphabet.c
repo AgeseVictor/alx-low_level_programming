851cd73f810410220600c7e53cd6b6c9f41f1ccc
AgeseVictor <agesevictor@gmail.com>
2022-07-05 18:24:35 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  *print_alphabet - Alphabet in lowercase
+ *   *
+ *    *Return:0
+ *     *
+ */
+void print_alphabet(void)
+{
+	char r;
+
+	for (r = 'a'; r <= 'z'; r++)
+	{
+		_putchar(r);
+	}
+	_putchar('\n');
+}
