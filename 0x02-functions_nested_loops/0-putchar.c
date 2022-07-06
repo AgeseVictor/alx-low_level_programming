4c98dec3a59c5fe5145e5c2235232cc0476a8b16
AgeseVictor <agesevictor@gmail.com>
2022-07-05 17:40:48 -0700

    First task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0(Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
