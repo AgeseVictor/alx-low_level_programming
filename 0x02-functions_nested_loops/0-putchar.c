c829d3394d23e917d557815ec6f01f628ec0887d
AgeseVictor <agesevictor@gmail.com>
2022-07-05 18:22:08 -0700

    omomo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,17 @@
-
 #include "main.h"
-
 /**
- * * main - prints putchar
- * * Return: always 0
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0(Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
 	_putchar('\n');
 
 	return (0);
-}}
+}
