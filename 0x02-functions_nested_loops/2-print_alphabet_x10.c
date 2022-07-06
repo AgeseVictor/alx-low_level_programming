fa824459c835ff64fdc6087a75a44b4fc7235213
AgeseVictor <agesevictor@gmail.com>
2022-07-05 18:26:33 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+nclude "main.h"
+
+/**
+ *  *print_alphabet_x10 - 10 times Alphabet in lowercase
+ *   *
+ *    *Return:0
+ *     *
+ */
+void print_alphabet_x10(void)
+{
+	char r;
+	int a = 0;
+
+	while (a < 10)
+	{
+		for (r = 'a'; r <= 'z'; r++)
+		{
+			_putchar(r);
+		}
+
+		_putchar('\n');
+		a++;
+	}
+}
