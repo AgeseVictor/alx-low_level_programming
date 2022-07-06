baabbf030b321d642fa0e7e889cbf31d93326a9a
AgeseVictor <agesevictor@gmail.com>
2022-07-05 17:48:27 -0700

    task 0 and wahala

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,14 @@
 
-#include "main.h"
+#include <unistd.h>
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: always 0(Success)
+ *  * _putchar - writes the character c to stdout
+ *   * @c: The character to print
+ *    *
+ *     * Return: On success 1.
+ *      * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
