fc9eac8fb2c6dd46c126a71657fa767c0a236417
AgeseVictor <agesevictor@gmail.com>
2022-07-05 18:46:50 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  * _abs - absolute value
+ *   * @n: integer
+ *    *
+ *     * Return: return absolute valuer of a number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
