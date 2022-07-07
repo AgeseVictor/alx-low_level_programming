dfb0edc9c3543ca3633178975bae7bbf8e4eb181
AgeseVictor <agesevictor@gmail.com>
2022-07-07 10:38:48 -0700

    i cold

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * * print_diagonal -> printing line
+ * * @n: integer params
+ */
+void print_diagonal(int n)
+{
+	int x, i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (i = 0; i < x; i++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
