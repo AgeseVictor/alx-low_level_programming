c22ba87d60fc58d25cb11b81abfd400e5fa3b25f
AgeseVictor <agesevictor@gmail.com>
2022-07-07 10:32:54 -0700

    thanks to God

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * * more_numbers -> printing more numbers
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if (y >= 10)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
