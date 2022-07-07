ff54ea27cf2717d3e36d466ea652eb187438f84d
AgeseVictor <agesevictor@gmail.com>
2022-07-07 10:35:25 -0700

    i'm fruitful

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * * print_line -> printing line
+ * * @n: integer params
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+			_putchar(95);
+		_putchar('\n');
+	}
+}
