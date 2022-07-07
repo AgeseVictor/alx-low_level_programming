a9c3a16883cc50387fbb0f8ab8e529c59b731cb1
VICTOR_AGESE <101999701+AgeseVictor@users.noreply.github.com>
2022-07-07 19:38:46 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,9 +2,9 @@
 #include <math.h>
 
 /**
- *  * main - finds and prints the largest prime factor of the number 612852475143
- *   * followed by a new line
- *    * Return: Always 0 (Success)
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
