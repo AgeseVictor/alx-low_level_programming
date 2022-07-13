d1a19d12a5a08659ff7107949a0a2734564d0669
AgeseVictor <agesevictor@gmail.com>
2022-07-12 19:00:40 -0700

    this is sweet

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *  * _atoi - convert string to int
+ *   * @s: string
+ *    * Return: int
+ */
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pt = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pt *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pt);
+}
