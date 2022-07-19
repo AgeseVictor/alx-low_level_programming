4d4fc86dc5190a235a72535ad246a5892263021b
AgeseVictor <agesevictor@gmail.com>
2022-07-18 17:10:37 -0700

    Thank you Jesus

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * * _strchr -> string character
+ * * @s: string given
+ * * @c: another char
+ * * Return: a string
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
+}
