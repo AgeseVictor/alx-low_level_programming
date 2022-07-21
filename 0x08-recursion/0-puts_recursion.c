a7bb723cb2b28db0605a9472e65217e222a02f55
AgeseVictor <agesevictor@gmail.com>
2022-07-20 17:18:58 -0700

    Last night committing

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * * _puts_recursion -> this is a function to print string recursivly
+ * * @s: string paramenter
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
