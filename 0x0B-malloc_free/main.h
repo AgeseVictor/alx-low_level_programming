e0fad81d96d716afeb0152ba4d41a208421a8331
AgeseVictor <agesevictor@gmail.com>
2022-07-25 17:58:17 -0700

    Thank you jesus

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdlib.h>
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+#endif
