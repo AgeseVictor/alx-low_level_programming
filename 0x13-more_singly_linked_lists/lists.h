622e8347a0196379673041fc37e33ab50681d73f
AgeseVictor <agesevictor@gmail.com>
2022-08-16 18:19:21 -0700

    listers

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,25 +1,24 @@
-#ifndef MORE_SINGLY_LINKED_LIST_H
-#define MORE_SINGLY_LINKED_LIST_H
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
 /**
- *  * struct listint_s - singly linked list
- *   * @n: integer
- *    * @next: points to the next node
- *     *
- *      * Description: singly linked list node structure
- *       * for Holberton project
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
+
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
-/* all other function prototypes */
-
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -35,4 +34,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
+
 #endif
