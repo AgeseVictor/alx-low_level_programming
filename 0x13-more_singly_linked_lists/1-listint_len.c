6475886c80edd799772e319f29352b0003f80060
Victor Agese <101999701+AgeseVictor@users.noreply.github.com>
2022-08-17 03:20:36 +0100

    tester

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: instance of struct list data type
+ * Return: number of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+    int nodes = 0;
+    for (nodes = 0; h; nodes++)
+        h = h->next;
+    return (nodes);
+}
