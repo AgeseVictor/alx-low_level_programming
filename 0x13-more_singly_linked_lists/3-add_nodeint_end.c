dc902d8e8611f357edd202eadd9717e29ea44791
AgeseVictor <agesevictor@gmail.com>
2022-08-18 16:20:40 -0700

    grace

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,21 +8,21 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new, *headcp = *head;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        return (NULL);
-    }
-    new->n = n;
-    new->next = NULL;
-    if (*head == NULL)
-    {
-        *head = new;
-        return (new);
-    }
-    while (headcp->next != NULL)
-        headcp = headcp->next;
-    headcp->next = new;
-    return (new);
+	listint_t *new, *headcp = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (headcp->next != NULL)
+		headcp = headcp->next;
+	headcp->next = new;
+	return (new);
 }
